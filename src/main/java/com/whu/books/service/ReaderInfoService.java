7ab9bd6260feb9912004881e1ad23398b95827a4
yuqi <yuqili_7@163.com>
2020-06-11 11:27:18 +0800

    version 1

---
+++ src/main/java/com/whu/books/service/ReaderInfoService.java
@@ -0,0 +1,35 @@
+package com.whu.books.service;
+
+import com.whu.books.dao.ReaderInfoDao;
+import com.whu.books.domain.ReaderInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class ReaderInfoService {
+
+    private ReaderInfoDao readerInfoDao;
+    @Autowired
+    public void setReaderInfoDao(ReaderInfoDao readerInfoDao) {
+        this.readerInfoDao = readerInfoDao;
+    }
+    public ArrayList<ReaderInfo> readerInfos(){
+        return readerInfoDao.getAllReaderInfo();
+    }
+
+    public boolean deleteReaderInfo(int readerId){
+        return readerInfoDao.deleteReaderInfo(readerId)>0;
+    }
+
+    public ReaderInfo getReaderInfo(int readerId){
+        return readerInfoDao.findReaderInfoByReaderId(readerId);
+    }
+    public boolean editReaderInfo(ReaderInfo readerInfo){
+        return readerInfoDao.editReaderInfo(readerInfo)>0;
+    }
+    public boolean addReaderInfo(ReaderInfo readerInfo){
+        return  readerInfoDao.addReaderInfo(readerInfo)>0;
+    }
+}
