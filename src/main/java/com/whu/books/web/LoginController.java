a20426a696e4a18e1ab98be24022560dbfbefc65
FnExpress <2016302580332@whu.edu.cn>
2020-06-11 17:47:48 +0800

    MySQL Driver

--- src/main/java/com/whu/books/web/LoginController.java
+++ src/main/java/com/whu/books/web/LoginController.java
@@ -28,11 +28,12 @@ public class LoginController {
     }
 
     //负责处理login.html请求
-    @RequestMapping(value = {"/","/login.html"})
-    public String toLogin(HttpServletRequest request){
+    @RequestMapping(value = {"/", "/login.html"})
+    public String toLogin(HttpServletRequest request) {
         request.getSession().invalidate();
         return "index";
     }
+
     @RequestMapping("/logout.html")
     public String logout(HttpServletRequest request) {
         request.getSession().invalidate();
@@ -44,34 +45,37 @@ public class LoginController {
     //请求参数会根据参数名称默认契约自动绑定到相应方法的入参中
     @RequestMapping(value = "/api/loginCheck", method = RequestMethod.POST)
     public @ResponseBody
-    Object loginCheck(HttpServletRequest request){
-        int id= Integer.parseInt(request.getParameter("id"));
+    Object loginCheck(HttpServletRequest request) {
+        int id = Integer.parseInt(request.getParameter("id"));
         String passwd = request.getParameter("passwd");
-                boolean isReader = loginService.hasMatchReader(id, passwd);
-                boolean isAdmin = loginService.hasMatchAdmin(id, passwd);
+        boolean isReader = loginService.hasMatchReader(id, passwd);
+        boolean isAdmin = loginService.hasMatchAdmin(id, passwd);
         HashMap<String, String> res = new HashMap<String, String>();
-                if (isAdmin==false&&isReader==false) {
-                    res.put("stateCode", "0");
-                    res.put("msg","账号或密码错误！");
-                } else if(isAdmin){
-                    Admin admin=new Admin();
-                    admin.setAdminId(id);
-                    admin.setPassword(passwd);
-                    request.getSession().setAttribute("admin",admin);
-                    res.put("stateCode", "1");
-                    res.put("msg","管理员登陆成功！");
-                }else {
-                    ReaderCard readerCard = loginService.findReaderCardByUserId(id);
-                    request.getSession().setAttribute("readercard", readerCard);
-                    res.put("stateCode", "2");
-                    res.put("msg","读者登陆成功！");
-                }
+        if (isAdmin == false && isReader == false) {
+            res.put("stateCode", "0");
+            res.put("msg", "账号或密码错误！");
+        } else if (isAdmin) {
+            Admin admin = new Admin();
+            admin.setAdminId(id);
+            admin.setPassword(passwd);
+            request.getSession().setAttribute("admin", admin);
+            res.put("stateCode", "1");
+            res.put("msg", "管理员登陆成功！");
+        } else {
+            ReaderCard readerCard = loginService.findReaderCardByUserId(id);
+            request.getSession().setAttribute("readercard", readerCard);
+            res.put("stateCode", "2");
+            res.put("msg", "读者登陆成功！");
+        }
         return res;
-    };
+    }
+
+    ;
+
     @RequestMapping("/admin_main.html")
     public ModelAndView toAdminMain(HttpServletResponse response) {
 
-            return new ModelAndView("admin_main");
+        return new ModelAndView("admin_main");
     }
 
 
@@ -82,7 +86,6 @@ public class LoginController {
     }
 
 
-
     @RequestMapping("/admin_repasswd.html")
     public ModelAndView reAdminPasswd() {
 
@@ -90,34 +93,35 @@ public class LoginController {
     }
 
     @RequestMapping("/admin_repasswd_do")
-    public String reAdminPasswdDo(HttpServletRequest request, String oldPasswd, String newPasswd, String reNewPasswd, RedirectAttributes redirectAttributes ) {
+    public String reAdminPasswdDo(HttpServletRequest request, String oldPasswd, String newPasswd, String reNewPasswd, RedirectAttributes redirectAttributes) {
 
-        Admin admin=(Admin) request.getSession().getAttribute("admin");
-        int id=admin.getAdminId();
-        String passwd=loginService.getAdminPasswd(id);
+        Admin admin = (Admin) request.getSession().getAttribute("admin");
+        int id = admin.getAdminId();
+        String passwd = loginService.getAdminPasswd(id);
 
-        if(passwd.equals(oldPasswd)){
-            boolean succ=loginService.adminRePasswd(id,newPasswd);
-            if (succ){
+        if (passwd.equals(oldPasswd)) {
+            boolean succ = loginService.adminRePasswd(id, newPasswd);
+            if (succ) {
 
                 redirectAttributes.addFlashAttribute("succ", "密码修改成功！");
                 return "redirect:/admin_repasswd.html";
-            }
-            else {
+            } else {
                 redirectAttributes.addFlashAttribute("error", "密码修改失败！");
                 return "redirect:/admin_repasswd.html";
             }
-        }else {
+        } else {
             redirectAttributes.addFlashAttribute("error", "旧密码错误！");
             return "redirect:/admin_repasswd.html";
         }
-    };
+    }
+
+    ;
 
     //配置404页面
-     @RequestMapping("*")
-     public String notFind(){
-     return "404";
-       }
+    @RequestMapping("*")
+    public String notFind() {
+        return "404";
+    }
 
 
 }
