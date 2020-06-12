1d7c46a0c0cab177b3a58cf2707cace242abe2d9
yuqi <yuqili_7@163.com>
2020-06-12 21:04:25 +0800

    MD5UtilsLogin

--- src/main/java/com/whu/books/repository/ReaderRepository.java
+++ src/main/java/com/whu/books/repository/ReaderRepository.java
@@ -11,4 +11,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
  
 public interface ReaderRepository extends JpaRepository<Reader, Long> {
+    Reader findByIDAndPassword(String phone, String password);
 }
