efe8cc120a305bdd5a1612381dd87b0414466bf7
FnExpress <2016302580332@whu.edu.cn>
2020-06-13 22:10:34 +0800

    测试md5

--- src/test/java/com/whu/books/BooksApplicationTests.java
+++ src/test/java/com/whu/books/BooksApplicationTests.java
@@ -1,5 +1,6 @@
 package com.whu.books;
 
+import com.whu.books.util.MD5Utils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,4 +12,15 @@ import java.sql.SQLException;
 @SpringBootTest
 class BooksApplicationTests {
 
+    /**
+     * 测试MD5加密密码功能
+     */
+    @Test
+    public void testMD5(){
+        String password = "123243546342";
+        System.out.println("password = " + password);
+        String encoded = MD5Utils.code(password);
+        System.out.println("encoded = " + encoded);
+        assert encoded.equals("087adceb109a6642c417d79fde3a78f5");
+    }
 }
